6aab1b052fb3aad992167232e4be5d4bb6921fe3
wrkbr <zcarc@naver.com>
2020-06-15 01:45:41 +0900

    styled-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import logo from './logo.svg'
 import './App.css';
-import Info from './info';
 
-function App() {
-  return <Info />;
+class App extends Component {
+  render() {
+
+    return (
+
+      <div className="App">
+        <header>
+
+          <img src={logo} className="logo" alt="logo" />
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+        </header>
+      </div>
+
+    );
+  }
 }
 
 export default App;
