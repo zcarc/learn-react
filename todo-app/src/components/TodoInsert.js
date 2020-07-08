60af22563751beb7a69b1eafe943b2a3f0639660
wrkbr <zcarc@naver.com>
2020-07-08 23:19:20 +0900

    기능 구현하기

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { MdAdd } from 'react-icons/md';
+import React, {useCallback, useState} from 'react';
+import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
-	return (
-		<form className={"TodoInsert"}>
-			<input placeholder={"할 일을 입력하세요"}/>
-			<button type={"submit"}>
-				{/*react-icons 페이지에서 원하는 아이콘 이름을 찾아서 불러옴*/}
-				<MdAdd />
-			</button>
-		</form>
-	);
+const TodoInsert = ({onInsert}) => {
+    console.log('TodoInsert');
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue(''); // value 값 초기화
+
+        e.preventDefault();
+    }, [onInsert, value]);
+
+    return (
+        <form className={"TodoInsert"} onSubmit={onSubmit}>
+            <input placeholder={"할 일을 입력하세요"}
+                   value={value}
+                   onChange={onChange}/>
+            <button type={"submit"}>
+                {/*react-icons 페이지에서 원하는 아이콘 이름을 찾아서 불러옴*/}
+                <MdAdd/>
+            </button>
+        </form>
+    );
 };
 
 export default TodoInsert;
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import TodoListItem from "./TodoListItem";
+import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
-    return (
-        <div className="TodoList">
-            <TodoListItem />
-            <TodoListItem />
-            <TodoListItem />
-        </div>
-    );
+const TodoList = ({ todos, onRemove, onToggle }) => {
+	return (
+		<div className="TodoList">
+			{todos.map((todo) => (
+				<TodoListItem
+					todo={todo}
+					key={todo.id}
+					onRemove={onRemove}
+					onToggle={onToggle}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default TodoList;
